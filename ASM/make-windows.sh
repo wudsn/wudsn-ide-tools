0f378e0dadfda2f2417399e78775ba9061274a79
Peter Dell <peter.dell@web.de>
2023-08-31 00:14:38 +0200

    Update ATASM and DASM build

--- ASM/make-windows.sh
+++ ASM/make-windows.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export OS="Windows"
-export EXT32="exe"
+export EXT="exe"
 
 echo Creating $OS binaries.
 
@@ -11,11 +11,11 @@ echo Creating $OS binaries.
 
 cd ATASM/src
 
-echo Creating ATASM - $OS 32-bit version
-export ARCH=-m32
+echo Creating ATASM - $OS 64-bit version
+export ARCH=-m64
 make
 chmod a+x atasm
-mv atasm ../atasm.$EXT32
+mv atasm ../atasm.$EXT
 make clean
 
 cd ../..
@@ -31,9 +31,13 @@ cd src
 echo Creating DASM - $OS 32-bit version
 export CC="gcc -m32"
 make
-mv dasm.exe   ../bin/dasm.$EXT32
-mv ftohex.exe ../bin/ftohex.$EXT32
+mv dasm.exe   ../bin/dasm.$EXT
+mv ftohex.exe ../bin/ftohex.$EXT
+
+# Rename bin to exclude it from clean.
+mv bin bin.bak
 make clean
+mv bin.bak bin
 cd ..
 
 cd test
@@ -49,7 +53,7 @@ cd ..
 cd MADS
 
 echo Creating MADS - $OS 32-bit version
-fpc -Mdelphi -v -O3 -omads.$EXT32 mads.pas
+fpc -Mdelphi -v -O3 -omads.$EXT mads.pas
 rm mads.o
 
 cd ..
@@ -58,6 +62,6 @@ cd ..
 # List result. 
 #------------------------------------------------------------------------
 echo Done.
-ls -al ATASM/*.$EXT32
-ls -al DASM/bin/*.$EXT32
-ls -al MADS/*.$EXT32
+ls -al ATASM/*.$EXT
+ls -al DASM/bin/*.$EXT
+ls -al MADS/*.$EXT
